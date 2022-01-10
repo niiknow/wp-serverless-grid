2ef4a34d249a53e85ff9f44eead5fcfff48238fa
noogen <tom@noogen.net>
2022-01-10 17:56:52 -0600

    update to jest 27

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     'tsx'
   ],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest'
